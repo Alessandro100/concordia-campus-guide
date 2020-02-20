0ada1c64e74c374253e9a38761659ba417339ec2
Alessandro <kalessandro14@gmail.com>
2020-02-20 11:01:10 -0500

    initialized google maps

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.search}>
+      </View>
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.mapStyle} 
+        />
     </View>
   );
 }
@@ -15,5 +21,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  search:{
+    width: Dimensions.get('window').width - 50,
+    height: 50,
+    backgroundColor: 'white',
+    position: "absolute",
+    zIndex: 2,
+    top: 50,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 1,  
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
