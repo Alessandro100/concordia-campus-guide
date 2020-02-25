e5a62fa151ceaef52db957c56650f8c2fa125854
Razine Bensari <bensaria97@gmail.com>
2020-02-25 16:07:09 -0500

    adding building coordinates

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View, Dimensions, Alert } from 'react-native';
+import MapView, {Polygon, PROVIDER_GOOGLE} from 'react-native-maps';
 import CampusToggleButton from './components/CampusToggleButton';
+import { buildingPolygons } from "./constants/CampusPolygons";
 
 class App extends Component {
+  constructor() {
+    // @ts-ignore
+      super();
 
-  state = {
-    region: {
-      // this is the SGW campus location
-      latitude: 45.497406,
-      longitude: -73.577102,
-      latitudeDelta: 0,
-      longitudeDelta: 0.01,
-    }
-  };
+    this.state = {
+        region: {
+            // this is the SGW campus location
+            latitude: 45.497406,
+            longitude: -73.577102,
+            latitudeDelta: 0,
+            longitudeDelta: 0.01,
+        },
+    };
+  }
+
 
   setMapLocation = (latitude, longitude) => {
     this.setState({
@@ -24,21 +30,41 @@ class App extends Component {
         longitudeDelta: 0.01,
       }
     })
-  }
+  };
+
+  getLocalPosition = () => {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+          const location = JSON.stringify(position);
+
+          this.setState({ location });
+        },
+        error => Alert.alert(error.message),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
 
   render() {
-    const { region } = this.state;
+    // @ts-ignore
+      const { region } = this.state;
 
     return (
-      <View style={styles.container}>
-        <View style={styles.search}></View>
-        <CampusToggleButton setMapLocation={this.setMapLocation} />
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={styles.mapStyle}
-          region={region}
-        />
-      </View>
+        <View style={styles.container}>
+          <View style={styles.search}/>
+          <CampusToggleButton setMapLocation={this.setMapLocation}/>
+          <MapView provider={PROVIDER_GOOGLE} style={styles.mapStyle} region={region}>
+              {buildingPolygons.map((polygon, index) => (
+                  <View key={index}>
+                      <Polygon
+                          coordinates={polygon}
+                          strokeColor='rgb(163, 9, 9)'
+                          strokeWidth={1}
+                          fillColor = 'rgba(163, 9, 9, 0.1)'
+                      />
+                  </View>
+              ))}
+          </MapView>
+        </View>
     );
   }
 }
