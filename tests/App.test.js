e05ba3e904b26b7e3896b31fb6c9036dd3044e09
wltrf <walter.fleury@gmail.com>
2020-02-29 09:33:59 -0500

    Added jest snapshots
    Installed react-test-renderer (jest snapshot).
    Wrote sample snapshot test.

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import CampusToggleButton from '../components/CampusToggleButton'
+
 describe('Sample Test Suite', () => {
   test('Sample Test Case', () => {
     expect(true).toEqual(true);
+  }),
+  test('Sample Snapshot Test', () => {
+    const tree = renderer
+      .create(<CampusToggleButton />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
