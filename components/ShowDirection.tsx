37b84feb8d1ea2d55bc38519f8e45c0753ef0bae
Alessandro <kalessandro14@gmail.com>
2020-02-29 18:22:34 -0500

    refactored showdirection [Issue #30]

--- components/ShowDirection.tsx
+++ components/ShowDirection.tsx
@@ -1,9 +1,16 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { StyleSheet, Image } from 'react-native';
 import Polyline from '@mapbox/polyline';
+import MapView, { Marker } from 'react-native-maps';
 import Colors from '../constants/Colors';
 import transportMode from '../classes/transportMode';
-import MapView, { Marker } from 'react-native-maps';
-import { Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  imageSize: {
+    width: 30,
+    height: 30,
+  },
+});
 
 type directionProps = {
   startLat: number;
@@ -13,27 +20,18 @@ type directionProps = {
   transportType: transportMode;
 };
 
-class ShowDirection extends Component<directionProps> {
-  state = {
-    coords: [],
-    steps: []
-  };
-
+class ShowDirection extends Component<directionProps, { steps: any[] }> {
   constructor(props) {
     super(props);
 
+    this.state = {
+      steps: [],
+    };
     this.getDirectionsSteps = this.getDirectionsSteps.bind(this);
   }
 
-  decodePoints(rawPoints) {
-    const points = Polyline.decode(rawPoints);
-    let coords = points.map((point, index) => {
-      return {
-        latitude: point[0],
-        longitude: point[1]
-      };
-    });
-    return coords;
+  componentDidMount() {
+    this.getDirectionsSteps();
   }
 
   // This function takes adresses as input and call the google API.
@@ -43,36 +41,41 @@ class ShowDirection extends Component<directionProps> {
     const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${startLat},${startLon}&destination=${endLat},${endLon}&mode=${transportType}&key=AIzaSyDpY_ACPWoo3mVPPmiLKQe1aBhjkDYlwJI`;
     const resp = await fetch(url);
     const respJson = await resp.json();
-    const response = respJson.routes[0];
-    const distanceTime = response.legs[0];
     // variables will be needed for future developement
-    const distance = distanceTime.distance.text;
-    const time = distanceTime.duration.text;
+    // const response = respJson.routes[0];
+    // const distanceTime = response.legs[0];
+    // const distance = distanceTime.distance.text;
+    // const time = distanceTime.duration.text;
     this.setState({ steps: respJson.routes[0].legs[0].steps });
   }
 
-  componentDidMount() {
-    this.getDirectionsSteps();
-  }
-
-  getPolylineColor(step) {
+  getPolylineColor = step => {
     if (step.travel_mode === 'TRANSIT') {
       return step.transit_details.line.color;
-    } else {
-      return Colors.mapsPolyline;
     }
-  }
+    return Colors.mapsPolyline;
+  };
 
-  getPinLocation(step) {
+  getPinLocation = step => {
     if (step) {
       return {
         latitude: step.end_location.lat,
-        longitude: step.end_location.lng
+        longitude: step.end_location.lng,
       };
-    } else {
-      return { latitude: 0, longitude: 0 };
     }
-  }
+    return { latitude: 0, longitude: 0 };
+  };
+
+  decodePoints = rawPoints => {
+    const points = Polyline.decode(rawPoints);
+    const coords = points.map(point => {
+      return {
+        latitude: point[0],
+        longitude: point[1],
+      };
+    });
+    return coords;
+  };
 
   render() {
     const { steps } = this.state;
@@ -80,33 +83,31 @@ class ShowDirection extends Component<directionProps> {
     const startingPinCoords = this.getPinLocation(steps[0]);
 
     return (
-      <Fragment>
+      <>
         <Marker
           coordinate={endingPinCoords}
-          title={'Ending Point'}
-          description={'Ending point of the trip'}
+          title="Ending Point"
+          description="Ending point of the trip"
         />
         <Marker
           coordinate={startingPinCoords}
-          title={'Starting Point'}
-          description={'Starting point of the trip'}
+          title="Starting Point"
+          description="Starting point of the trip"
           opacity={0.7}
         >
-          <Image
-            source={require('../assets/starting_icon.png')}
-            style={{ width: 30, height: 30 }}
-          />
+          <Image source={require('../assets/starting_icon.png')} style={styles.imageSize} />
         </Marker>
 
         {steps.map(step => (
           // @ts-ignore
           <MapView.Polyline
+            key={`${step.end_location.lat}-${step.end_location.lng}`}
             strokeWidth={4}
             strokeColor={this.getPolylineColor(step)}
             coordinates={this.decodePoints(step.polyline.points)}
           />
         ))}
-      </Fragment>
+      </>
     );
   }
 }
