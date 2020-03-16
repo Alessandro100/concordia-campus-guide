122e48777b36a2c6203accfc6497c1146f5b08f8
Alessandro <kalessandro14@gmail.com>
2020-03-16 11:32:57 -0400

    temporary app.tsx to test indoordirections [Issue #31]

--- App.tsx
+++ App.tsx
@@ -12,6 +12,9 @@ import OutdoorPOI from './classes/outdoorPOI';
 import { Svg, Line } from 'react-native-svg';
 import IndoorFloor from './classes/indoorFloor';
 import PolygonsAndMarkers from './components/PolygonsAndMarkers';
+//services
+import IndoorFloorService from './services/indoorFloorService';
+import BuildingService from './services/buildingService';
 
 const styles = StyleSheet.create({
   container: {
@@ -54,6 +57,9 @@ class App extends Component<{}, appState> {
   constructor(props) {
     super(props);
 
+    BuildingService.init();
+    IndoorFloorService.init();
+
     this.state = {
       region: {
         // this is the SGW campus location
@@ -78,103 +84,24 @@ class App extends Component<{}, appState> {
     });
   };
 
-  setIndoorTile(arrayWidth, arrayHeight, imageHeight, initialHeight, path:[{x:number, y:number}], imageWidth) {
-    let array = [];
-    const widthPerTile = imageWidth / arrayWidth; // in px
-    const heightPerTile = imageHeight / arrayHeight;
-
-    for(let i = 0; i < arrayHeight; i++) {
-      let yPosition = (i * heightPerTile) + initialHeight
-      for(let z = 0; z < arrayWidth; z++){
-        let xPosition = z * widthPerTile;
-        array.push(
-          <View key={i + " " + z} style={{zIndex: 5, position: 'absolute', top: yPosition, left: xPosition, borderRadius: 2, backgroundColor: 'red', width: widthPerTile, height: heightPerTile, opacity: 0.5}}>
-          </View>
-        )
-      }
-    }
-    return array;
-  }
-
-  drawLines(imageHeight, path:[{x:number, y:number}], widthPerTile, heightPerTile, initialHeight, imageWidth) {
-    let lines = []
-    
-    for(let pathIndex = 0; pathIndex < path.length - 1; pathIndex++) {
-      const xPosition = path[pathIndex].x * widthPerTile;
-      const yPosition = path[pathIndex].y * heightPerTile;
-
-      const startingLocation = {
-        xpx: path[pathIndex].x * widthPerTile + (0.5 * widthPerTile),  
-        ypx: path[pathIndex].y * heightPerTile + (0.5 * heightPerTile) 
-      }
-      const endingLocation = {
-        xpx: path[pathIndex + 1].x * widthPerTile + (0.5 * widthPerTile), 
-        ypx: path[pathIndex + 1].y * heightPerTile + (0.5 * heightPerTile)
-      }
-      lines.push(
-        <Svg height={imageHeight} width={imageWidth} style={{zIndex: 6, top: initialHeight, position: 'absolute'}} origin="0, 0" >
-          <Line x1={startingLocation.xpx} y1={startingLocation.ypx} x2={endingLocation.xpx} y2={endingLocation.ypx}  stroke="blue" strokeWidth="2" />
-        </Svg>
-      )
-    }
-    return lines;
-  }
-
   render() {
     const { region, markers, polygons } = this.state;
 
-    const arrayWidth = 30;
-    const arrayHeight = 30;
     const windowWidth = Dimensions.get('window').width;
-    const windowHeigh = Dimensions.get('window').height;
     const imageRatio = 2823.0 / 2643.0;
     const imageHeight = windowWidth * imageRatio;
+    //let floor = new IndoorFloor(require('./assets/indoor-floor-plans/Hall-1.png'), windowWidth, imageHeight, 1, 100, 0);
 
-    //new image
-    const imageRatio2 = 1704.0 / 4944.0;
-    const imageHeight2 = windowWidth * imageRatio2;
-
-    const widthPerTile = windowWidth / arrayWidth; // in px
-    const heightPerTile = imageHeight / arrayHeight;
-    const initialHeight = 100;
-
-    //2643w × 2823h
-    //4944 × 1704
-    /*
-      {x: 5, y:29}, 
-      {x: 5, y:26}, 
-    */
-
-    const fakePath = [
-      {x: 8, y:26},
-      {x: 25, y:26},
-      {x: 25, y:17},
-      {x: 10, y:17},
-      {x: 10, y:10},
-    ]
-
-    let floor = new IndoorFloor(require('./assets/indoor-floor-plans/Hall-1.png'), windowWidth, imageHeight, 1, 100, 0);
-    let floor2 = new IndoorFloor(require('./assets/indoor-floor-plans/CC1.png'), windowWidth, imageHeight2, 1, 100, 0); 
+    let floor = IndoorFloorService.getFloor('Hall', 1);
+    floor.setImageWidth(windowWidth);
+    floor.setInitialHeightPosition(100);
 
     return (
-      <View style={styles.container}>
-        {/* <Image
-          resizeMode={'cover'}
-          style={{width: windowWidth, height: imageHeight, position: "absolute", top: initialHeight, left: 0, zIndex: 4}}
-          source={require('./assets/indoor-floor-plans/Hall-1.png')}
-        />
-        {this.setIndoorTile(arrayWidth, arrayHeight, imageHeight, initialHeight, fakePath, windowWidth)}
-        {this.drawLines(imageHeight, fakePath, widthPerTile, heightPerTile, initialHeight, windowWidth)} */}
+      <View style={styles.container}>        
         {floor.showFloorImage()}
         {floor.showIndoorTile()}
+        {floor.drawPath('fake', 'nothing')}
         
-        {/* <SvgUri
-          width="200"
-          height="200"
-          source={{
-            uri: './assets/indoor-floor-plans/Hall-1.png',
-          }}
-        /> */}
         {/* <View style={styles.search} />
         <CampusToggleButton setMapLocation={this.setMapLocation} />
         <MapView provider={PROVIDER_GOOGLE} style={styles.mapStyle} region={region}>
