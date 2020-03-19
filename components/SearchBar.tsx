650ccae4cc84b11591f85cbbcee2d5917edbb7e8
Marwan <marwan.a011@mail.com>
2020-03-19 19:22:03 -0400

    fix styles in search bar to display list view below search bar (issue #38)

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import {Alert, Dimensions, StyleSheet, View} from "react-native";
+import {Alert, Dimensions, StyleSheet,Text,Image, View} from "react-native";
 import Colors from "../constants/Colors";
 import Location from '../classes/location';
 
@@ -8,10 +8,29 @@ const homePlace = { description: 'Home', geometry: { location: { lat: 48.8152937
 const workPlace = { description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }};
 
 const styles = StyleSheet.create({
-    textInputContainer: {
-        backgroundColor: 'rgba(0,0,0,0)',
+
+    container:{
+        width: Dimensions.get('window').width - 50,
+        position: 'absolute',
+        zIndex: 3,
+        top: 50,
+    },
+    
+    row:{
+        backgroundColor: Colors.white,
+    },
+    textInputContainer:{
+        backgroundColor:Colors.white,
         borderTopWidth: 0,
-        borderBottomWidth:0
+        borderBottomWidth:0,
+        shadowColor: Colors.black,
+        shadowOffset: {
+            width: 1,
+            height: 1,
+        },
+        shadowOpacity: 0.15,
+        shadowRadius: 1.84,
+        elevation: 2,
     },
     description: {
         fontWeight: 'bold',
@@ -20,28 +39,9 @@ const styles = StyleSheet.create({
         color: Colors.searchBarPlaces,
     },
     listView: {
-        color: 'black', //To see where exactly the list is
-        zIndex: 16, //To popover the component outwards
+        color: Colors.black, //To see where exactly the list is
         position: 'absolute',
-    },
-    search: {
-        width: Dimensions.get('window').width - 50,
-        height: 50,
-        backgroundColor: Colors.white,
-        position: 'absolute',
-        zIndex: 2,
-        top: 50,
-        borderRadius: 5,
-        shadowColor: Colors.black,
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.8,
-        shadowRadius: 1,
-    },
-    searchBox: {
-        top: 0,
-        position: "absolute",
-        flex: 1,
-        justifyContent: 'center',
+        top:40,
     },
 });
 
@@ -50,7 +50,8 @@ type searchBarProps = {
 };
 
 type searchBarState = {
-    location: Location
+    latitude:number;
+    longitude:number;
 }
 
 class SearchBar extends Component<searchBarProps, searchBarState> {
@@ -68,7 +69,7 @@ class SearchBar extends Component<searchBarProps, searchBarState> {
     };
    render(){
        return (
-           <View style={styles.search}>
+        
                <GooglePlacesAutocomplete
                    placeholder='Search'
                    minLength={2} // minimum length of text to search
@@ -109,7 +110,7 @@ class SearchBar extends Component<searchBarProps, searchBarState> {
                        rankby: 'distance',
                        type: 'cafe'
                    }}
-
+                   
                    GooglePlacesDetailsQuery={{
                        // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
                        //fields: 'formatted_address',
@@ -117,12 +118,12 @@ class SearchBar extends Component<searchBarProps, searchBarState> {
 
                    filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
                    predefinedPlaces={[homePlace, workPlace]}
-
+                   clearButtonMode="always"
                    debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-                   // renderLeftButton={()  => <Image source={require('path/custom/left-icon')} />}
-                   // renderRightButton={() => <Text>Custom text after the input</Text>}
+                    //renderLeftButton={()  => <Image source={require('../assets/search.png')} />}
+                   // renderRightButton={() => <Text>Custom text after the input</Text>} 
                />
-           </View>
+         
        );
    }
 }
