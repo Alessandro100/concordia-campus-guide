3318f965e99a6058b312c2def12ab835e4ff5723
Alessandro <kalessandro14@gmail.com>
2020-03-28 10:28:17 -0400

    Enforced typing for floorData files [Issue #31]

--- classes/indoorFloorData.ts
+++ classes/indoorFloorData.ts
@@ -1,38 +1,59 @@
 import Coordinate from './coordinate';
 
 class IndoorFloorData {
-    floorImage: any;
-    imageWidthPX: number;
-    imageHeightPX: number;
-    floorNumber: string;
-    buildingName: string;
-    walkways: {topLeft: Coordinate, bottomRight: Coordinate}[];
-    elevator: Coordinate;
-    staircase: Coordinate;
-    bathrooms: Coordinate[];
-    entrance: Coordinate;
-    waterFountains: Coordinate[];
-    classRooms: {classRoom: string, location: Coordinate}[];
-    
-    constructor(floorImage, floorNumber, buildingName, imageWidthPX, imageHeightPX, elevatorData, walkwayData, staircaseData, bathroomData, entranceData, waterFountainsData, classroomsData) {
-        this.floorImage = floorImage;
-        this.floorNumber = floorNumber;
-        this.buildingName = buildingName;
-        this.imageWidthPX = imageWidthPX;
-        this.imageHeightPX = imageHeightPX;
-        this.walkways = walkwayData;
-        this.bathrooms = bathroomData;
-        this.waterFountains = waterFountainsData;
-        this.classRooms = classroomsData;
-        this.staircase = staircaseData;
-        this.elevator = elevatorData;
-        if (floorNumber === 1) {
-            this.entrance = entranceData;
-        }
-        else this.entrance = null;
+  floorImage: any;
 
-    }
+  imageWidthPX: number;
+
+  imageHeightPX: number;
+
+  floorNumber: string;
+
+  buildingName: string;
+
+  walkways: { topLeft: Coordinate; bottomRight: Coordinate }[];
+
+  elevator: Coordinate;
+
+  staircase: Coordinate;
+
+  bathrooms: Coordinate[];
+
+  entrance: Coordinate;
+
+  waterFountains: Coordinate[];
+
+  classRooms: { classRoom: string; location: Coordinate }[];
+
+  constructor(
+    floorImage: any,
+    floorNumber: any,
+    buildingName: string,
+    imageWidthPX: number,
+    imageHeightPX: number,
+    elevatorData: Coordinate,
+    walkwayData: { topLeft: Coordinate; bottomRight: Coordinate }[],
+    staircaseData: Coordinate,
+    bathroomData: Coordinate[],
+    entranceData: Coordinate,
+    waterFountainsData: Coordinate[],
+    classroomsData: { classRoom: string; location: Coordinate }[]
+  ) {
+    this.floorImage = floorImage;
+    this.floorNumber = floorNumber;
+    this.buildingName = buildingName;
+    this.imageWidthPX = imageWidthPX;
+    this.imageHeightPX = imageHeightPX;
+    this.walkways = walkwayData;
+    this.bathrooms = bathroomData;
+    this.waterFountains = waterFountainsData;
+    this.classRooms = classroomsData;
+    this.staircase = staircaseData;
+    this.elevator = elevatorData;
+    if (floorNumber === 1) {
+      this.entrance = entranceData;
+    } else this.entrance = null;
+  }
 }
 
 export default IndoorFloorData;
-
