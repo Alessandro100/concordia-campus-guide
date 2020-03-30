fe8c67cd403f30061b8c3c5a63bb8b846ec65692
arthur tourneyrie <arthur.tourneyrie@gmail.com>
2020-03-30 15:30:34 -0400

    cleaned up EpicExploreCampus.test.js added comment and renamed properly

--- tests/EpicExploreCampusMap.test.js
+++ tests/EpicExploreCampusMap.test.js
@@ -13,7 +13,7 @@ import PolygonsAndMarkers from '../components/PolygonsAndMarkers';
 import CampusPolygons from '../constants/CampusPolygons';
 import App from '../App';
 import CurrentPosition from '../components/CurrentPosition'
-// import BottomDrawerBuilding from '../components/BottomDrawerBuilding';
+// import BottomDrawerBuilding from '../components/BottomDrawerBuilding'; Error importing bottomDrawer-rn module 
 import SearchBar from '../components/SearchBar'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import ShowDirection from '../components/ShowDirection'
@@ -39,16 +39,16 @@ function getShuttleBusMarkers() {
 }
 
 
-// function displayCurrentLocation() {
-//     const options = {
-//         enableHighAccuracy: true,
-//         timeOut: 20000,
-//         maximumAge: 60 * 60 * 24,
-//     };
-//     navigator.geolocation.getCurrentPosition(this.success, null, options);
-// }
+function displayCurrentLocation() {
+    const options = {
+        enableHighAccuracy: true,
+        timeOut: 20000,
+        maximumAge: 60 * 60 * 24,
+    };
+    navigator.geolocation.getCurrentPosition(this.success, null, options);
+}
 
-describe('Epic 1 System Test test suite', () => {
+describe('Epic 1 System-Test test suite', () => {
     test('US-18 - toggle button navigates through campuses', () => {
         const wrapperButton = shallow(<CampusToggleButton
             setMapLocation={function setMapLocation(lat, lon) { }}
@@ -57,7 +57,7 @@ describe('Epic 1 System Test test suite', () => {
         wrapperButton.find('[testID="toggle-loyola-button"]').props().onPress();
         expect(wrapperButton.state().currentCampusView).toEqual('Loyola');
     });
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
     test('US-19 As a user, I want to see additional information for each building.', () => {
         const buildings = obtainBuildings();
         const polygons = CampusPolygons.slice(0);
@@ -74,43 +74,12 @@ describe('Epic 1 System Test test suite', () => {
         )
         wrapperBuilding.find('[testID="LB"]').props().onPress();
     });
-    // bottom drawer building IS FUCKED
-    //     test('US-22 As a user, I want to be able to click on a building from the map view of either SGW or Loyola campus.', () => {
-    //         const wrapperDrawer = mount(
-    //             <BottomDrawerBuilding
-    //             />
-    //         )
 
-    //         //wrapperDrawer.find('[testID="LB"]').props().onPress();
-    //         wrapperDrawer.find('[testID="gayCuck"]').props().onCollapse();
-    //         expect(wrapperDrawer.displayBuildingInfo()).toHaveBeenCalled(1);
-    //     }
-    //     )
     test('US-24 verify the button to access current location loads', done => {
         const wrapperMount = mount(<CurrentPosition />);
         expect(wrapperMount.find(TouchableOpacity)).toHaveLength(1);
         done();
     });
-   // ALESSANDRO HALP
-    // test('US-23 On press show current location', () => {
-    //     const mockGeolocation = {
-    //         getCurrentPosition: jest.fn(),
-    //       };
-
-    //       global.navigator.geolocation = mockGeolocation;
-    //       const displayCurrentLocation = jest.fn();
-    //     const wrapperMount = shallow (<CurrentPosition/>);
-    //     const instance = wrapperMount.instance();
-    //     wrapperMount.find('[testID="KYS"]').props().onPress();
-    //     expect(instance.displayCurrentLocation).toHaveBeenCalled();
-    //  });
-
-    // bottom drawer building IS FUCKED
-    // test('US-32 verify the button to transtion to indoor-navigation exists', done => {
-    //     const wrapperMount = mount(<BottomDrawerBuilding />);
-    //     expect(wrapperMount.find('[testID="indoor-navigation"]').find(Button)).toHaveLength(1);
-    //     done();
-    //   }); 
 
 
     test('US-38 I want to be able to use the search bar', done => {
@@ -166,10 +135,6 @@ describe('Epic 1 System Test test suite', () => {
         done();
     });
 
-    // test('US-89 I want to see events when I click on a building', () => {
-    // const wrapperMount = mount()
-
-    // })
 
     test('US-53 By the click of a button I want to open a navigation menu', done => {
         const wrapperMount = mount(<NavBtn />);
@@ -179,3 +144,42 @@ describe('Epic 1 System Test test suite', () => {
 
     
 });
+
+// Potential future test which aren't working right now for specific reasons
+
+    // bottom drawer-rn problem with JEST
+    // Will Have to Investigate
+    //     test('US-22 As a user, I want to be able to click on a building from the map view of either SGW or Loyola campus.', () => {
+    //         const wrapperDrawer = mount(
+    //             <BottomDrawerBuilding
+    //             />
+    //         )
+
+    //         //wrapperDrawer.find('[testID="LB"]').props().onPress();
+    //         wrapperDrawer.find('[testID="gayCuck"]').props().onCollapse();
+    //         expect(wrapperDrawer.displayBuildingInfo()).toHaveBeenCalled(1);
+    //     }
+    //     )
+
+       
+    // Test Currently does not work
+    // test('US-23 On press show current location', () => {
+    //     const mockGeolocation = {
+    //         getCurrentPosition: jest.fn(),
+    //       };
+
+    //       global.navigator.geolocation = mockGeolocation;
+    //       const displayCurrentLocation = jest.fn();
+    //     const wrapperMount = shallow (<CurrentPosition/>);
+    //     const instance = wrapperMount.instance();
+    //     wrapperMount.find('[testID="diplayCurrentLocation"]').props().onPress();
+    //     expect(instance.displayCurrentLocation).toHaveBeenCalled();
+    //  });
+
+    // bottom drawer-rn problem with JEST
+    // Will Have to Investigate
+    // test('US-32 verify the button to transtion to indoor-navigation exists', done => {
+    //     const wrapperMount = mount(<BottomDrawerBuilding />);
+    //     expect(wrapperMount.find('[testID="indoor-navigation"]').find(Button)).toHaveLength(1);
+    //     done();
+    //   }); 
