156924959d0fd233664e0d3d6d3aa2fa81b4639e
landry333 <57639988+Landry333@users.noreply.github.com>
2020-04-13 20:05:37 -0400

    Add file for static markers
    Static markers for out door map

---
+++ constants/OutCampusInterestMarkers.tsx
@@ -0,0 +1,135 @@
+const OutCampusInterestMarkers = [
+
+    {
+      coordinate: {
+        latitude: 45.495340,
+        longitude: -73.577360,
+      },
+      name: 'Pharmaprix',
+      type:'store',
+      address: '1500 Saint-Catherine St W, Montreal, Quebec H3G 1S8',
+      icon: 'store',
+      pinColor:'red',
+    },
+      {
+      coordinate: {
+        latitude: 45.496228,
+        longitude: -73.580266,
+      },
+      name: 'RBC Royal Bank',
+      type:'bank',
+      address: '2157 Guy St, Montreal, Quebec H3H 2L9',
+      icon: 'bank',
+      pinColor:'gold',
+    },
+
+    {
+      coordinate: {
+        latitude: 45.455826,
+        longitude: -73.641090,
+      },
+      name: 'CIBC Bank',
+      type:'bank',
+      address: '7400 Rue Sherbrooke Ouest, Montréal, QC H4B 1R7',
+      icon: 'bank',
+      pinColor:'gold',
+    },
+
+    {
+      coordinate: {
+        latitude: 45.495642,
+        longitude: -73.577030,
+      },
+      name: 'Mc Donald',
+      type:'Fast Food',
+      address: '1472 Saint-Catherine St W, Montreal, Quebec H3G 1S8',
+      icon: 'food',
+      pinColor:'blue',
+    },
+
+    {
+      coordinate: {
+        latitude: 45.455125,
+        longitude: -73.641085,
+      },
+      name: 'Subway',
+      type:'Fast Food',
+      address: '7420 Rue Sherbrooke Ouest, Montréal, QC H4B 1R7',
+      icon: 'food',
+      pinColor:'teal',
+    },
+
+    {
+      coordinate: {
+        latitude: 45.456129,
+        longitude: -73.640886,
+      },
+      name: 'Second Cup',
+      type:'Cafe',
+      address: '7335-7345 Rue Sherbrooke Ouest, Montréal, QC H4B 1R9',
+      icon: 'coffee-outline',
+      pinColor:'blue',
+    },
+
+    {
+      coordinate: {
+        latitude: 45.495909,
+        longitude: -73.576813,
+      },
+      name: 'Koa Lua Sainte Catherine',
+      type:'Restaurant',
+      address: '1446 Saint-Catherine St W, Montreal, Quebec H3G 1R3',
+      icon: 'food-fork-drink',
+      pinColor:'indigo',
+    },
+    
+    {
+      coordinate: {
+        latitude: 45.459881,
+        longitude: -73.636970,
+      },
+      name: 'Soulvaki George',
+      type:'Restaurant',
+      address: '6995 Av de Monkland Montréal, QC H4B 1R1 ',
+      icon: 'food-fork-drink',
+      pinColor:'indigo',
+    },
+
+    {
+      coordinate: {
+        latitude: 45.497743,
+        longitude: -73.578617,
+      },
+      name: 'Ramen Misoya',
+      type:'Restaurant',
+      address: '2065A Bishop St, Montreal, Quebec H3G 2E8',
+      icon: 'food-fork-drink',
+      pinColor:'indigo',
+    },
+    {
+      coordinate: {
+        latitude: 45.495743,
+        longitude: -73.579045,
+      },
+      name: 'Guy Concordia station',
+      type:'metro station',
+      address: '1445 Rue Guy, Montréal, QC H3H 2L5',
+      icon: 'subway',
+      pinColor:'navy',
+    },
+
+    {
+      coordinate: {
+        latitude: 45.454395,
+        longitude: -73.641294,
+      },
+      name: 'Gare Montréal-Ouest (Elmhurst/Sherbrooke)',
+      type:'train station',
+      address: '',
+      icon: 'train',
+      pinColor:'navy',
+    },
+  ];
+  
+  export default OutCampusInterestMarkers;
+  
