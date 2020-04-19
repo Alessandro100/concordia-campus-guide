d9e90423329d2d6c4988f4f8264a00567dd4b23d
Razine Bensari <bensaria97@gmail.com>
2020-04-19 17:11:06 -0400

    fixing merge conflict (issue #25)

--- components/PolygonsAndMarkers.tsx
+++ components/PolygonsAndMarkers.tsx
@@ -1,19 +1,19 @@
-
-import React, { Component } from 'react';
-import { Marker, Polygon } from 'react-native-maps';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import Colors from '../constants/Colors';
-import ShuttleBusMarkers from '../constants/CampusShuttleBusStop';
-import Building from '../classes/building';
+import React, { Component } from "react";
+import { Marker, Polygon } from "react-native-maps";
+import { Image, StyleSheet, Text, View } from "react-native";
+import {ColorPicker} from "../constants/Colors";
+import colorBlindMode from '../classes/colorBlindMode';
+import ShuttleBusMarkers from "../constants/CampusShuttleBusStop";
+import Building from "../classes/building";
 import {
   obtainCoordinateFromBuilding,
   parseLocationToLatLngType,
-} from '../services/buildingService';
-import Location from '../classes/location';
-import Campus from '../classes/campus';
-import GoogleMapsAdapter from '../classes/googleMapsAdapter';
-
-const busIcon = require('./../assets/shuttle_bus_icon.png');
+} from "../services/buildingService";
+import Location from "../classes/location";
+import Campus from "../classes/campus";
+import GoogleMapsAdapter from "../classes/googleMapsAdapter";
+let Colors = ColorPicker(colorBlindMode.normal);
+const busIcon = require("./../assets/shuttle_bus_icon.png");
 
 const styles = StyleSheet.create({
   circle: {
@@ -60,7 +60,8 @@ class PolygonsAndMarkers extends Component<markersAndPolygonsProps, markersAndPo
     this.state = {
       buildings,
       polygons,
-      building: new Building('', '', [], null, '', null, ''),
+      colorBlindMode,
+      building: new Building("", "", [], null, "", null, ""),
       location: new Location(0, 0),
       campus: new Campus(null, '', ''),
       shuttleBusMarkers: ShuttleBusMarkers.slice(0),
@@ -95,80 +96,80 @@ class PolygonsAndMarkers extends Component<markersAndPolygonsProps, markersAndPo
       placesCoordinates,
     } = this.state;
     const { places } = this.props;
-    Colors = ColorPicker(colorBlindMode)
+    Colors = ColorPicker(colorBlindMode);
     return (
-      <View>
-        {polygons.map(polygon => (
-          <View key={`${String(polygon.latitude)}-${String(polygon.longitude)}`}>
-            <Polygon
-              coordinates={polygon}
-              strokeColor={Colors.polygonStroke}
-              strokeWidth={1}
-              fillColor={Colors.polygonFill}
-            />
-          </View>
-        ))}
-        {buildings.map(buildingMarker => (
-          <Marker
-            testID={buildingMarker.getIdentifier()}
-            key={buildingMarker.getIdentifier()}
-            coordinate={parseLocationToLatLngType(obtainCoordinateFromBuilding(buildingMarker))}
-            title={buildingMarker.getIdentifier()}
-            description={buildingMarker.getName()}
-            pinColor={Colors.markersPinColor}
-            onPress={() => {
-              location.setLatitude(buildingMarker.getLocation().getLatitude());
-              location.setLongitude(buildingMarker.getLocation().getLongitude());
-              campus.setLocation(buildingMarker.getLocation());
-              building.setName(buildingMarker.getName());
-              building.setDescription(buildingMarker.getDescription());
-              building.setEvents(buildingMarker.getEvents()); // TODO: change for event feature
-              building.setTitle(buildingMarker.title);
-              building.setCampus(campus);
-              building.setLocation(location);
-              building.setIdentifier(buildingMarker.getIdentifier());
-              this.displayBuildingInfo(building, true);
-            }}
-          >
-            <View style={styles.circle}>
-              <Text style={styles.pinText}>{buildingMarker.getIdentifier()}</Text>
-            </View>
-          </Marker>
-        ))}
-        {shuttleBusMarkers.map(marker => (
-          <Marker
-            key={marker.title}
-            coordinate={marker.coordinate}
-            title={marker.title}
-            description={marker.description}
-          >
-            <Image source={busIcon} style={styles.icon} />
-          </Marker>
-        ))}
+        <View>
+          {polygons.map(polygon => (
+              <View key={`${String(polygon.latitude)}-${String(polygon.longitude)}`}>
+                <Polygon
+                    coordinates={polygon}
+                    strokeColor={Colors.polygonStroke}
+                    strokeWidth={1}
+                    fillColor={Colors.polygonFill}
+                />
+              </View>
+          ))}
+          {buildings.map(buildingMarker => (
+              <Marker
+                  testID={buildingMarker.getIdentifier()}
+                  key={buildingMarker.getIdentifier()}
+                  coordinate={parseLocationToLatLngType(obtainCoordinateFromBuilding(buildingMarker))}
+                  title={buildingMarker.getIdentifier()}
+                  description={buildingMarker.getName()}
+                  pinColor={Colors.markersPinColor}
+                  onPress={() => {
+                    location.setLatitude(buildingMarker.getLocation().getLatitude());
+                    location.setLongitude(buildingMarker.getLocation().getLongitude());
+                    campus.setLocation(buildingMarker.getLocation());
+                    building.setName(buildingMarker.getName());
+                    building.setDescription(buildingMarker.getDescription());
+                    building.setEvents(buildingMarker.getEvents()); // TODO: change for event feature
+                    building.setTitle(buildingMarker.title);
+                    building.setCampus(campus);
+                    building.setLocation(location);
+                    building.setIdentifier(buildingMarker.getIdentifier());
+                    this.displayBuildingInfo(building, true);
+                  }}
+              >
+                <View style={styles.circle}>
+                  <Text style={styles.pinText}>{buildingMarker.getIdentifier()}</Text>
+                </View>
+              </Marker>
+          ))}
+          {shuttleBusMarkers.map(marker => (
+              <Marker
+                  key={marker.title}
+                  coordinate={marker.coordinate}
+                  title={marker.title}
+                  description={marker.description}
+              >
+                <Image source={busIcon} style={styles.icon} />
+              </Marker>
+          ))}
 
-        {places.map(marker => (
-          <Marker
-            key={marker.id}
-            coordinate={placesCoordinates.parseGoogleMapLocation(marker.geometry.location)}
-            title={marker.name}
-            description={marker.formatted_address}
-            pinColor={Colors.primaryColor}
-            onPress={() => {
-              location.setLatitude(
-                placesCoordinates.parseGoogleMapLocation(marker.geometry.location).latitude
-              );
-              location.setLongitude(
-                placesCoordinates.parseGoogleMapLocation(marker.geometry.location).longitude
-              );
-              building.setIdentifier(marker.id);
-              building.setLocation(location);
-              building.setDescription(marker.formatted_address);
-              building.setName(marker.name);
-              this.displayPlace(building, true);
-            }}
-          />
-        ))}
-      </View>
+          {places.map(marker => (
+              <Marker
+                  key={marker.id}
+                  coordinate={placesCoordinates.parseGoogleMapLocation(marker.geometry.location)}
+                  title={marker.name}
+                  description={marker.formatted_address}
+                  pinColor={Colors.primaryColor}
+                  onPress={() => {
+                    location.setLatitude(
+                        placesCoordinates.parseGoogleMapLocation(marker.geometry.location).latitude
+                    );
+                    location.setLongitude(
+                        placesCoordinates.parseGoogleMapLocation(marker.geometry.location).longitude
+                    );
+                    building.setIdentifier(marker.id);
+                    building.setLocation(location);
+                    building.setDescription(marker.formatted_address);
+                    building.setName(marker.name);
+                    this.displayPlace(building, true);
+                  }}
+              />
+          ))}
+        </View>
     );
   }
 }
